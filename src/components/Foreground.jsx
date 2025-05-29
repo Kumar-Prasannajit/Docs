ee77965fe71fdfbb5fd52b7028888c63a9becb04
Kumar Prasannajit Sahu <kumarprasannajitsahu@gmail.com>
2025-05-29 21:20:49 +0530

    added some css

--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Foreground() {
   return (
-    <div className='fixed top-0 left-0 z-[3] w-full h-full'>
+    <div className='fixed top-0 left-0 z-[4] w-full h-full'>
         
     </div>
   )
