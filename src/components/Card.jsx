42b06eb9ac5d553abc1ae0bb11d02d062b6c426b
Kumar Prasannajit Sahu <kumarprasannajitsahu@gmail.com>
2025-06-04 22:30:46 +0530

    added card in foreground

---
+++ src/components/Card.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { FaRegFileAlt } from "react-icons/fa";
+
+export const Card  = () => {
+  return (
+    <div className='w-50 h-60 rounded-[20px] bg-zinc-900/90 text-white p-5'>
+        <FaRegFileAlt />
+        <p className='text-sm leading-none mt-5 font-semibold   '>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+    </div>
+  )
+}
--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Card } from './Card'
 
 function Foreground() {
   return (
     <div className='fixed top-0 left-0 z-[4] w-full h-full'>
-        
+        <Card />
     </div>
   )
 }
