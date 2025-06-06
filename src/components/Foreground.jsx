589eff862a9ab92bb4abc033f335ef17d6a8ec4c
Kumar Prasannajit Sahu <kumarprasannajitsahu@gmail.com>
2025-06-06 15:44:53 +0530

    completed the docs project

--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -1,10 +1,45 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { Card } from './Card'
 
 function Foreground() {
+const ref = useRef(null)
+const data = [
+  {
+    desc: "This is demo data for card 1.",
+    filesize: ".9mb",
+    close: true,
+    tag: {
+      isOpen: true,
+      tagTitle: "Download Now",
+      tagColor: "green"
+    }
+  },
+  {
+    desc: "This is demo data for card 1.",
+    filesize: ".9mb",
+    close: true,
+    tag: {
+      isOpen: true,
+      tagTitle: "Download Now",
+      tagColor: "blue"
+    }
+  },
+  {
+    desc: "This is demo data for card 1.",
+    filesize: ".9mb",
+    close: true,
+    tag: {
+      isOpen: false,
+      tagTitle: "Download Now",
+      tagColor: "blue"
+    }
+  }
+]
   return (
-    <div className='fixed top-0 left-0 z-[4] w-full h-full'>
-        <Card />
+    <div ref={ref} className='fixed top-0 left-0 z-[4] w-full h-full flex gap-10 flex-wrap p-5'>
+        {data.map((item, index) => (
+          <Card data={item} reference={ref}/>
+        ))}
     </div>
   )
 }
